da8ae6b679cfc0a2ab19f0cf76b28c43a3fc7a19
Sreenivassreee <ksreenivascse@gmail.com>
2021-05-17 20:13:44 +0530

    screen 1 completed with listing the data

--- FileGalary/ViewController.swift
+++ FileGalary/ViewController.swift
@@ -7,19 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
-    let allItems = [String]()
+var allItems = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath:path)
-
+        allItems = items
     }
 
-    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allItems.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
+        cell.textLabel?.text = allItems[indexPath.row]
+        return cell
+    }
 }
 
